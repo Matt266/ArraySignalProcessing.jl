abace1c2edd103e48893adc8f08c9b1758056bb1
Matthias Schön <matt@matthias-schoen.de>
2025-08-10 13:32:20 +0200

    Reworked Wavefront types to be more performant

--- src/core/Wavefront/AzEl.jl
+++ src/core/Wavefront/AzEl.jl
@@ -1,27 +1,29 @@
-struct AzEl <: PlaneWave
-    coords::AbstractMatrix
-    function AzEl(coords)
-         # coords = [azimtuh, elevation] 2xD matrix
-
-        if ndims(coords) == 0
-            coords = [coords; 0]
-        end
-
-        # only when single az/el pair
-        # transpose changes ndims 
-        # az-only: [az0, az2]', az/el-pair: [az, el]
-        # ndims(transpose([3, 4])) is 2
-        # ndims([3, 4]) is 1
-        if ndims(coords) == 1
-            coords = reshape(coords, 2, 1)
-        end
-
+struct AzEl{T} <: PlaneWave where T <: AbstractMatrix
+    coords::T
+    function AzEl(coords::T) where T <: AbstractMatrix
+        # coords = [azimuths...; elevations...] 2xD matrix
         M, D = size(coords)
 
+        M <= 0 && throw(DomainError(M, "'coords' must have at least one row."))
+        M >= 3 && throw(DomainError(M, "'coords' must have at most two rows: [azimuths...; elevations...]"))
+        D <= 0 && throw(DomainError(D, "'coords' must have at least one column"))
+
         if M == 1
-            coords = [coords; zeros(1, D)]
+            padding = similar(coords, 1, D)
+            fill!(padding, zero(eltype(coords)))
+            coords = vcat(coords, padding)
         end
 
-        return new(coords)
+        return new{typeof(coords)}(coords)
     end
+end
+
+# list of azimtuhs
+function AzEl(azimuths::T) where T <: AbstractVector
+    return AzEl(reshape(azimuths, 1, :))
+end
+
+# list of azimtuhs
+function AzEl(azimuths::T...) where T <: Number
+    return AzEl(collect(azimuths))
 end
--- src/core/Wavefront/WaveVec.jl
+++ src/core/Wavefront/WaveVec.jl
@@ -1,28 +1,33 @@
-struct WaveVec <: PlaneWave
-    coords::AbstractMatrix
-    function WaveVec(coords)
+struct WaveVec{T} <: PlaneWave where T <: AbstractMatrix
+    coords::T
+    function WaveVec(coords::T) where T <: AbstractMatrix
         # coords = [kx; ky; kz] 3xD matrix
 
-        if ndims(coords) == 0
-            coords = [coords; 0; 0]
-        end
-
-        # only when single k vector
-        # transpose changes ndims
-        # single k vector: [kx, ky, kz]'
-        # multiple kx: [kx, kx, kx]
-        if ndims(coords) == 1
-            coords = reshape(coords, 3, 1)
-        end
-
         M, D = size(coords)
+        M <= 0 && throw(DomainError(M, "'coords' must have at least one row."))
+        M >= 4 && throw(DomainError(M, "'coords' must have at most three rows: [kx...; ky...; kz...]"))
+        D <= 0 && throw(DomainError(D, "'coords' must have at least one column"))
 
         if M == 1
-            coords = [coords; zeros(2, D)]
+            padding = similar(coords, 2, D)
+            fill!(padding, zero(eltype(coords)))
+            coords = vcat(coords, padding)
         elseif M==2
-            coords = [coords; zeros(1, D)]
-        end 
+            padding = similar(coords, 1, D)
+            fill!(padding, zero(eltype(coords)))
+            coords = vcat(coords, padding)
+        end
 
-        return new(coords)
+        return new{typeof(coords)}(coords)
     end
+end
+
+# list of kx
+function WaveVec(kx_vec::T) where T <: AbstractVector
+    return WaveVec(reshape(kx_vec, 1, :))
+end
+
+# list of kx
+function WaveVec(kx::T...) where T <: Number
+    return WaveVec(collect(kx))
 end
