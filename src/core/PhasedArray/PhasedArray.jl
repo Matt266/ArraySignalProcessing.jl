c45c3c0a5c0f90a412359f149b6907e5dc11f056
Matthias Schön <matt@matthias-schoen.de>
2025-08-11 08:00:29 +0200

    first rework of NestedArray

--- src/core/PhasedArray/PhasedArray.jl
+++ src/core/PhasedArray/PhasedArray.jl
@@ -7,6 +7,10 @@ end
 struct NestedArray{E<:AbstractPhasedArray, S<:AbstractVector{<:AbstractPhasedArray}} <: AbstractPhasedArray
     elements::E
     subarrays::S
+    function NestedArray(elements::E, subarrays::S) where {E<:AbstractPhasedArray, S<:AbstractVector{<:AbstractPhasedArray}}
+        (length(elements) == length(subarrays)) || throw(DimensionMismatch("Length of elements ($(length(elements))) and number of subarrays ($(length(subarrays))) do't match)"))
+        return new{E, S}(elements, subarrays)
+    end
 end
 
 function Base.length(pa::PhasedArray)
@@ -25,7 +29,12 @@ end
 function steer(pa::NestedArray, args...; kwargs...)
     v_super = steer(pa.elements, args...; kwargs...)
     v_sub = map(sa -> steer(sa, args...; kwargs...), pa.subarrays)
-    return reduce(vcat, map((sup_row, sub_mat) -> sub_mat .* reshape(sup_row, 1, :), eachrow(v_super), v_sub))
+
+    v_sub_stacked = vcat(v_sub...)
+    indices = reduce(vcat, [fill(i, l) for (i, l) in enumerate(length.(pa.subarrays))])
+    v_super_expanded = v_super[indices, :]
+
+    return v_sub_stacked .* v_super_expanded
 end
 
 IsotropicArray(args...; kwargs...) = PhasedArray(IsotropicArrayManifold(args...; kwargs...))  
