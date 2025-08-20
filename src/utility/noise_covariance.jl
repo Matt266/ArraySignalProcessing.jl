5d8d98ca982326a4d5a5ec928e5eacfbe8c83e7c
Matthias Schön <matt@matthias-schoen.de>
2025-08-20 10:30:15 +0200

    Added wideband LCMV example using TappedDelayLines. Used quick fix for diffnoise which needs to be revised!

--- src/utility/noise_covariance.jl
+++ src/utility/noise_covariance.jl
@@ -26,3 +26,21 @@ function diffnoise(pa::AbstractPhasedArray, σ², f, c=c_0)
     #σ²*Γ.(Ref(pa), n, n', Ref(k))
     return σ² .* [Γ(i, j) for i in n, j in n] 
 end
+
+# TODO: this is a quick fix to have diffnoise work with TappedDelayLines
+#       References are needed to check how diffnoise is modelled
+#       for taps correctly!
+function diffnoise(pa::TappedDelayLine, σ², f, c=c_0)
+    J = pa.num_taps
+    Ts = 1 / pa.fs
+    ω = 2π*f
+
+    # diffuse spatial noise correlation
+    R_spatial = diffnoise(PhasedArray(pa.manifold), σ², f, c)
+
+    # temporal correlation between taps  
+    R_time = [exp(-1im * ω * (i-j) * Ts) for i=0:J-1, j=0:J-1]
+
+    # extend to TDL: Kronecker product with I_J
+    return kron(R_time, R_spatial)
+end
