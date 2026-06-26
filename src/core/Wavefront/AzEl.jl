330d12af54fbc600337a8cab1de49d1e784aa72a
Matthias Schön <matt@matthias-schoen.de>
2026-06-26 17:42:11 +0200

    refinements to AzEl

--- src/core/Wavefront/AzEl.jl
+++ src/core/Wavefront/AzEl.jl
@@ -1,28 +1,30 @@
-struct AzEl{T<:AbstractMatrix} <: PlaneWave
-    coords::T
-    function AzEl(coords::AbstractMatrix)
-        # coords = [azimuths...; elevations...] 2xD matrix
-        M, D = size(coords)
-
-        M <= 0 && throw(DimensionMismatch("'coords' has size $(size(coords)) but must have at least one row."))
-        M >= 3 && throw(DimensionMismatch("'coords' has size $(size(coords)) but must have at most two rows: [azimuths...; elevations...]"))
-        D <= 0 && throw(DimensionMismatch("'coords' has size $(size(coords)) but must have at least one column"))
-
-        if M == 1
-            padded_coords = vcat(coords, zeros(eltype(coords), 1, D))
-        else
-            padded_coords = vcat(coords, zeros(eltype(coords), 0, D))
-        end
-
-        return new{typeof(padded_coords)}(padded_coords)
-    end
+struct AzEl{T, A<:AbstractMatrix{T}} <: PlaneWave
+    coords::A
 end
-# list of azimuths
+
+Adapt.@adapt_structure AzEl
+
+function AzEl(coords::AbstractMatrix)
+    M, D = size(coords)
+
+    M <= 0 && throw(DimensionMismatch("'coords' has size $(size(coords)) but must have at least one row."))
+    M >= 3 && throw(DimensionMismatch("'coords' has size $(size(coords)) but must have at most two rows: [azimuths...; elevations...]"))
+    D <= 0 && throw(DimensionMismatch("'coords' has size $(size(coords)) but must have at least one column"))
+
+    padded_coords = similar(coords, eltype(coords), 2, D)
+    padded_coords[1:M, :] = coords
+    
+    if M == 1
+        padded_coords[2:2, :] .= 0
+    end
+
+    return AzEl{eltype(padded_coords), typeof(padded_coords)}(padded_coords)
+end
+
 function AzEl(azimuths::AbstractVector)
     return AzEl(reshape(azimuths, 1, :))
 end
 
-# list of azimuths
 function AzEl(azimuths...)
     return AzEl(collect(promote(azimuths...)))
 end
