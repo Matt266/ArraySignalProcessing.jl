c6e61af675748fd1523eecd66c12ee6c12ea55ff
Matthias Schön <matt@matthias-schoen.de>
2026-06-26 13:25:34 +0200

    changed SampledArrayManifold to be consistent with new Manifold API

--- src/core/ArrayManifold/SampledArrayManifold.jl
+++ src/core/ArrayManifold/SampledArrayManifold.jl
@@ -13,45 +13,48 @@ single frequency are recorded, responses reduces to:
 You must sample over at least one dimension. If other dependencies (e.g., temperature) are 
 be needed, either create an array of SampledArrayManifolds or implement a custom ArrayManifold subtype.
 """
-struct SampledArrayManifold{NT<:Int, MT<:AbstractInterpolation, PT<:AbstractInterpolation} <: AbstractArrayManifold
+struct SampledArrayManifold{NT, CT, KA, MT, PT} <: AbstractArrayManifold
     num_elements::NT
-    coords_type::DataType
-    keep_axes::BitVector
+    coords_type::CT
+    keep_axes::KA
     itp_mag::MT
     itp_phase::PT
-    function SampledArrayManifold(responses::AbstractArray; c_grid=[0], f_grid=[0], coords_grid=AzEl(0))
-        num_elements = size(responses)[end]
+end
 
-        # sort all axes
-        coords_perms = sortperm.(unique.(eachrow(coords_grid.coords)))
-        coords_axes = map(i -> unique(eachrow(coords_grid.coords)[i])[coords_perms[i]], 1:length(coords_perms))
+Adapt.@adapt_structure SampledArrayManifold
 
-        c_perms = sortperm(c_grid)
-        c_grid = c_grid[c_perms]
+function SampledArrayManifold(responses::AbstractArray; c_grid=[0], f_grid=[0], coords_grid=AzEl(0))
+    num_elements = size(responses)[end]
 
-        f_perms = sortperm(f_grid)
-        f_grid = f_grid[f_perms]
+    # sort all axes
+    coords_perms = sortperm.(unique.(eachrow(coords_grid.coords)))
+    coords_axes = map(i -> unique(eachrow(coords_grid.coords)[i])[coords_perms[i]], 1:length(coords_perms))
 
-        element_perms = Vector(1:num_elements)
-        element_axes = Vector(1:num_elements)
+    c_perms = sortperm(c_grid)
+    c_grid = c_grid[c_perms]
 
-        # tuple with all axes of length >1
-        grid_perms = [c_perms, f_perms, coords_perms..., element_perms]
-        grid_axes = [c_grid, f_grid, coords_axes..., element_axes]
-        keep_axes = length.(grid_axes) .> 1
-        grid_perms = (grid_perms[keep_axes]...,)
-        grid_axes = (grid_axes[keep_axes]...,)
+    f_perms = sortperm(f_grid)
+    f_grid = f_grid[f_perms]
 
-        # responses with all axes of length >1
-        flattened_responses = dropdims(responses; dims=Tuple(findall(size(responses) .<= 1)))
+    element_perms = Vector(1:num_elements)
+    element_axes = Vector(1:num_elements)
 
-        # permute responsens to match sorted axes again
-        flattened_responses = flattened_responses[grid_perms...]
-        
-        itp_mag = extrapolate(interpolate(grid_axes, abs.(flattened_responses), Gridded(Interpolations.Linear())),Interpolations.Flat())
-        itp_phase = extrapolate(interpolate(grid_axes, angle.(flattened_responses), Gridded(Interpolations.Linear())),Interpolations.Flat())
-        return new{typeof(num_elements), typeof(itp_mag), typeof(itp_phase)}(num_elements, typeof(coords_grid), keep_axes, itp_mag, itp_phase)
-    end
+    # tuple with all axes of length >1
+    grid_perms = [c_perms, f_perms, coords_perms..., element_perms]
+    grid_axes = [c_grid, f_grid, coords_axes..., element_axes]
+    keep_axes = length.(grid_axes) .> 1
+    grid_perms = (grid_perms[keep_axes]...,)
+    grid_axes = (grid_axes[keep_axes]...,)
+
+    # responses with all axes of length >1
+    flattened_responses = dropdims(responses; dims=Tuple(findall(size(responses) .<= 1)))
+
+    # permute responsens to match sorted axes again
+    flattened_responses = flattened_responses[grid_perms...]
+    
+    itp_mag = extrapolate(interpolate(grid_axes, abs.(flattened_responses), Gridded(Interpolations.Linear())),Interpolations.Flat())
+    itp_phase = extrapolate(interpolate(grid_axes, angle.(flattened_responses), Gridded(Interpolations.Linear())),Interpolations.Flat())
+    return SampledArrayManifold(num_elements, typeof(coords_grid), keep_axes, itp_mag, itp_phase)
 end
 
 # TODO: implement Wavefront type conversion logic.
@@ -59,12 +62,24 @@ end
 # which for now is the intended behaviour 
 # TODO: probably in each Wavefront constructer: constrain values (e.g., azimuth to -π...+π and elevation to 0...π).
 function (a::SampledArrayManifold)(angles::Wavefront, f::Number, c::Number=c_0)
-    angles = convert(a.coords_type, angles)
+    angles_conv = convert(a.coords_type, angles)
+
+    f_vec = f isa Number ? (f,) : f
+    c_vec = c isa Number ? (c,) : c
+
+    combinations = Iterators.product(eachcol(angles_conv.coords), f_vec, c_vec)
 
     # matrix of tuples: each row an element index e, each col an column from the angles 
     # all axes with size <= 1 will be dropped from the points
-    query_points = hcat(map(col -> map(e -> Tuple([c, f, col..., e][a.keep_axes]), 1:a.num_elements), eachcol(angles.coords))...)
-    return map(query_point -> (a.itp_mag(query_point...)*exp(1im*a.itp_phase(query_point...))), query_points)
+    A_matrix = [ 
+        begin
+            query_point = Tuple([c_val, f_val, col..., e][a.keep_axes])
+            a.itp_mag(query_point...) * exp(1im * a.itp_phase(query_point...))
+        end
+        for e in 1:a.num_elements, (col, f_val, c_val) in combinations 
+    ]
+    
+    return A_matrix
 end
 
 function Base.length(a::SampledArrayManifold)
